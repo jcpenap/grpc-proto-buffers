dc6195c98ada8518d5456dafae888a61db35f10d
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2022-10-04 18:44:49 -0500

    Serialization and Deserialization

--- src/main/kotlin/com/vinsguru/protobuf/PersonDemo.kt
+++ src/main/kotlin/com/vinsguru/protobuf/PersonDemo.kt
@@ -1,6 +1,10 @@
 package com.vinsguru.protobuf
 
 import com.vinsguru.models.Person
+import java.io.File
+import java.nio.file.Files
+import kotlin.io.path.Path
+import kotlin.io.path.writeBytes
 
 fun main(args: Array<String>) {
     val person = Person.newBuilder()
@@ -8,4 +12,20 @@ fun main(args: Array<String>) {
         .setAge(10)
         .build()
     println(person.toString())
+
+    objectToBytes(person)
+    bytesToObject()
+
+}
+
+private fun bytesToObject() {
+    val path = Path("sam.ser")
+    val bytes = Files.readAllBytes(path)
+    val newPerson = Person.parseFrom(bytes)
+    println(newPerson)
+}
+
+private fun objectToBytes(person: Person) {
+    val path = Path("sam.ser")
+    path.writeBytes(person.toByteArray())
 }
