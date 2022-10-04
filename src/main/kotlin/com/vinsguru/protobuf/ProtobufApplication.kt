5725a61d43b3d1a7d0fde8fc464e35eaa08f1586
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2022-10-04 18:04:09 -0500

    First commit

---
+++ src/main/kotlin/com/vinsguru/protobuf/ProtobufApplication.kt
@@ -0,0 +1,11 @@
+package com.vinsguru.protobuf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProtobufApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProtobufApplication>(*args)
+}
---
+++ src/test/kotlin/com/vinsguru/protobuf/ProtobufApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vinsguru.protobuf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProtobufApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
