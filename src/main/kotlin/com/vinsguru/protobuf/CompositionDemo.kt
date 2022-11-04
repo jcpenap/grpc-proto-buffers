81959583c852de0b9dd7daa1c0ef54881688e916
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2022-11-04 18:41:24 -0500

    Creating a composition Demo

---
+++ src/main/kotlin/com/vinsguru/protobuf/CompositionDemo.kt
@@ -0,0 +1,30 @@
+package com.vinsguru.protobuf
+
+import com.vinsguru.models.Address
+import com.vinsguru.models.Car
+import com.vinsguru.models.Person
+
+fun main(args: Array<String>) {
+
+    val address = Address.newBuilder()
+        .setPostbox(123)
+        .setStreet("main street")
+        .setCity("Atlanta")
+        .build()
+
+    val car = Car.newBuilder()
+        .setMake("Honda")
+        .setModel("Accard")
+        .setYear(2020)
+        .build()
+
+    val juan = Person.newBuilder()
+        .setName("Juan")
+        .setAge(37)
+        .setAddress(address)
+        .setCar(car)
+        .build()
+
+    println(juan)
+
+}
