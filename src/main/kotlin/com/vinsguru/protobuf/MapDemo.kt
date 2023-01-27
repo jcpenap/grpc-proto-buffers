fe64ac821b8ad08ce9e0e1073a753f718b0f8b70
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2023-01-27 16:11:47 -0500

    Adding a enum demo

--- src/main/kotlin/com/vinsguru/protobuf/MapDemo.kt
+++ src/main/kotlin/com/vinsguru/protobuf/MapDemo.kt
@@ -1,5 +1,6 @@
 package com.vinsguru.protobuf
 
+import com.vinsguru.models.BodyStyle
 import com.vinsguru.models.Car
 import com.vinsguru.models.Dealer
 
@@ -8,12 +9,14 @@ fun main(args: Array<String>) {
     val accord = Car.newBuilder()
         .setMake("Honda")
         .setModel("Accord")
+        .setBodyStyle(BodyStyle.COUPE)
         .setYear(2020)
         .build()
 
     val civic = Car.newBuilder()
         .setMake("Honda")
         .setModel("Civic")
+        .setBodyStyle(BodyStyle.SEDAN)
         .setYear(2005)
         .build()
 
@@ -22,6 +25,6 @@ fun main(args: Array<String>) {
         .putModel(2020, accord)
         .build()
 
-    println(dealer.modelMap)
+    println(dealer.getModelOrThrow(2020).bodyStyle)
 
 }
